bbcb361ecf661131b8b9d082aad124ef4cdeff9b
Sergey Kazmin <kazmin5796@mail.ru>
2023-05-14 20:49:56 +0300

    create project

---
+++ src/test/java/ru/coda/traineeship/traineeship/TraineeshipApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.coda.traineeship.traineeship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
